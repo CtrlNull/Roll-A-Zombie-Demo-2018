6c233c85bd3eaba489504d6929bada23ce22ca39
Ctrl_Null <raphael@fonseca.tech>
2019-01-13 11:35:41 -0500

    stuck on dispearing bug

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,20 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     public int selectedZombiePosition;
     public GameObject selectedZombie;
     public List<GameObject> zombies;
     public Vector3 selectedSize;
     public Vector3 defaultSize;
 
-     void Start() {
-         SelectZombie(zombies[0], 0);
-     }
+    void Start () {
+        SelectZombie (zombies[0], 0);
+    }
+    void Update () {
+        if (Input.GetKeyDown ("left")) {
+            ShiftLeft ();
+        }
+        else if (Input.GetKeyDown ("right")) {
+            ShiftRight ();
+        }
+    }
 
-     void SelectZombie(GameObject newZombie, int newPosition) {
-         selectedZombie = newZombie;
-         selectedZombie.transform.localScale = selectedSize;         
-     }
+    void ShiftLeft () {
+        // Z1, Z2, Z3, Z4
+        if (selectedZombiePosition == 0) {
+            SelectZombie (zombies[3], 3);
+        } else {
+            SelectZombie (zombies[selectedZombiePosition - 1], selectedZombiePosition - 1);
+        }
+    }
+
+    void ShiftRight () {
+        if (selectedZombiePosition == 3) {
+            SelectZombie (zombies[0], 0);
+        } else {
+            SelectZombie (zombies[selectedZombiePosition + 1], selectedZombiePosition + 1);
+        }
+    }
+
+    void SelectZombie (GameObject newZombie, int newPosition) {
+        selectedZombie.transform.localScale = defaultSize;
+        selectedZombie = newZombie;
+        selectedZombie.transform.localScale = selectedSize;
+        selectedZombiePosition = newPosition;
+    }
 }
